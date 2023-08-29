0336fd5f347c5bce05b71f19468f26ab37d0233b
octocatblain <blainmasaku2@gmail.com>
2023-08-29 14:26:23 +0300

    navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,37 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-    <a class="navbar-brand" href="/">{{ config('app.name', 'LSAPP') }}</a>
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item">
-        <a class="nav-link" href="/">Home
-            {{-- <span class="sr-only">(current)</span> --}}
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/posts">Blog</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/about">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/services">Services</a>
-      </li>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <a class="navbar-brand" href="/">{{ config('app.name', 'LSAPP') }}</a>
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" href="/">Home
+                    {{-- <span class="sr-only">(current)</span> --}}
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/posts">Blog</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/services">Services</a>
+            </li>
 
-      {{-- <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li> --}}
-    </ul>
-    <ul class="navbar-nav navbar-right mr-3">
-        <li class="nav-item"><a class="nav-link" href="/posts/create">Create Post</a></li>
-    </ul>
-    
-    {{-- search form --}}
-    {{-- <form class="form-inline my-2 my-lg-0">
+        </ul>
+
+        {{-- navbar right side --}}
+        <ul class="navbar-nav navbar-right mr-3">
+            <li class="nav-item"><a class="nav-link" href="/posts/create">Create Post</a></li>
+        </ul>
+
+
+
+        {{-- search form --}} {{-- <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> --}}
-  </div>
+    </div>
 </nav>
